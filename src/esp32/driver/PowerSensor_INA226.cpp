7cdf96e967f07dc67ec15bfe35e4ae51a966d043
Arief Nur Misuari <arief.nur@efishery.com>
2024-10-13 18:27:54 +0700

    feat(PowerSensor) : implement power sensor driver

--- src/esp32/driver/PowerSensor_INA226.cpp
+++ src/esp32/driver/PowerSensor_INA226.cpp
@@ -9,6 +9,7 @@ bool PowerSensor_INA226::init()
     log_d("INA226 lib version: %s", INA226_LIB_VERSION);
 
     if (!Wire.begin(6, 7))
+    // if (!Wire.begin(21, 22))
     {
         log_e("failed to begin I2C");
         return false;
@@ -25,12 +26,14 @@ bool PowerSensor_INA226::init()
     return true;
 }
 
-// float PowerSensor_INA226::readVoltage()
-// {
-//     return 0.0;
-// }
+float PowerSensor_INA226::readVoltage()
+{
+    log_d("read voltage");
+    return INA.getBusVoltage();
+}
 
-// uint32_t PowerSensor_INA226::batPercent()
-// {
-//     return 0;
-// }
+uint32_t PowerSensor_INA226::batPercent()
+{
+    return map(readVoltage(), 10, 13, 0, 100);
+    // return (readVoltage() - 11) / (12 - 11) * 100;
+}
--- src/esp32/driver/PowerSensor_INA226.h
+++ src/esp32/driver/PowerSensor_INA226.h
@@ -9,8 +9,8 @@ class PowerSensor_INA226 : public PowerSensorInterface
     public:
         PowerSensor_INA226();
         bool init();
-        // float readVoltage();
-        // uint32_t batPercent();
+        float readVoltage();
+        uint32_t batPercent();
 
     private:
         INA226 INA{0x40};
